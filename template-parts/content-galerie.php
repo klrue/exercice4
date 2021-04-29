1967a5389d69410eb1a90a9a2e0005f8e6e77ecf
klrue <kevin.larue@outlook.com>
2021-04-29 12:49:46 -0400

    first commit

---
+++ template-parts/content-galerie.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template part l'affichage des bloc de cours dans front-page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package theme4w4
+ */
+
+?>
+<article class="flip-card">
+	<div class="flip-card-inner">
+  		<div class="flip-card-front">
+ 			 <?php the_post_thumbnail('thumbnail'); ?>
+  		</div>
+  		<div class="flip-card-back">
+	  		<p><a href="<?php echo get_permalink() ?>"><?php the_title(); ?> </a></p>
+  		</div>
+	</div>
+	
+</article>
